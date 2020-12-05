1f066854f3d931e0228b834002a3efdfdbfcab73
Janaina Sanches <janasanches1@outlook.com>
2020-12-05 13:22:08 -0300

    corrigindo erros

--- routes/presentes.js
+++ routes/presentes.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/Presentes', function(req, res, next) {
+  res.render('Presentes')
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,23 +1,4 @@
-const express = require('express');
-const app = express();
-app.use(express.static('public'))
-app.set('view engine', 'ejs')
-let active = " "
-app.get('/', function (req, resp) {
-    active=' active'
-    resp.render('index',{active})
-})
-app.get('/Presentes', function (req, resp) {
-    active=' active'
-    resp.render('Presentes',{active})
-})
-app.get('/Utilidades', function (req, resp) {
-    active=' active'
-    resp.render('Utilidades',{active})
-})
-
-
-
+const app= require('./app')
 app.listen(process.env.PORT ||3000, ()=>{
-    console.log("Servidor executando em:Porta 3000, http://localhost:3000")
+    console.log("Servidor ex'ecutando em:Porta 3000, http://localhost:3000")
 })
